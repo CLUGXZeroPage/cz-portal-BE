d996c1195e3a4c99e2eda11a0aaad066e3712dc0
YamYamee <82762402+YamYamee@users.noreply.github.com>
2025-02-27 18:09:23 +0900

    Feat/init/1 (#11)
    * fix: 초기 값 null 없게 수정
    
    * fix: 에러 코드 수정
    
    * fix: 검증 로직 추가
    
    * fix: 검증 로직 추가
    
    * fix: 파싱 수정
    
    * fix: 경로 수정
    
    * fix: 에러 코드 수정
    
    * fix: 닉네임 최대 10자까지
    
    * fix: 입력 조건 수정

--- src/main/java/com/example/czportalpage/info/dto/InfoPostDto.java
+++ src/main/java/com/example/czportalpage/info/dto/InfoPostDto.java
@@ -3,6 +3,7 @@ package com.example.czportalpage.info.dto;
 //객체를 새로 만들때 사용하는 DTO
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,7 +14,8 @@ public class InfoPostDto {
     @NotBlank
     @Size(max = 20)
     private String username;
+
     @NotBlank
-    @Size(max = 10)
+    @Pattern(regexp = "^[가-힣]{1,6}$|^[a-zA-Z]{1,10}$", message = "한글은 최대 6자, 영어는 최대 10자까지 허용됩니다.")
     private String nickname;
 }
