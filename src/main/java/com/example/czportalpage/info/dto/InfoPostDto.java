3a2c87d9398bd155d1ce07ca0c7d23d1a043d59c
YamYamee <cartcnx@naver.com>
2025-02-27 16:14:27 +0900

    fix: 닉네임 최대 10자까지

--- src/main/java/com/example/czportalpage/info/dto/InfoPostDto.java
+++ src/main/java/com/example/czportalpage/info/dto/InfoPostDto.java
@@ -3,6 +3,7 @@ package com.example.czportalpage.info.dto;
 //객체를 새로 만들때 사용하는 DTO
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +11,9 @@ import lombok.Setter;
 public class InfoPostDto {
 
     @NotBlank
+    @Size(max = 20)
     private String username;
     @NotBlank
+    @Size(max = 10)
     private String nickname;
 }
